e088092f622eff1d69a610637b9ba96764ce6773
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-10-05 16:48:34 -0300

    feat: (_app.tsx) :sparkles: First style configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logo from '../assets/images/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles()
-  
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+      <Header>
+        <img src={logo.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
