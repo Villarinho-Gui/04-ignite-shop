07c3d9481f4e100492e75e3e9105b98b99dd0bf3
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-10-11 13:00:11 -0300

    feat: (index.tsx) :sparkles: Adding link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from "../styles/pages/home"
+import Link from 'next/link'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
@@ -32,14 +33,19 @@ export default function Home({ products }: HomeProps) {
       {
         products.map(product => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
         })
       }
