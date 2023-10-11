0fdf2a7f8714dc8689313b4cf8d548ec9a9f9160
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-10-11 14:41:33 -0300

    feat: ([id].tsx) :sparkles: First product configuration

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+
+            <ProductDetails>
+                <h1>camiseta x</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, animi ducimus! Aliquam similique reiciendis non culpa debitis. Consectetur dicta, nesciunt non ullam deleniti tenetur minus, voluptatibus iste ut itaque adipisci?</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
